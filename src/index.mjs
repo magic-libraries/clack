78cb33a6342c214d856b5d38f1ffcaa10920a961
jascha <jascha@jaeh.at>
2021-01-05 19:47:58 +0100

    add responseType, timeoutSeconds, timeout handler

--- src/index.mjs
+++ src/index.mjs
@@ -1,13 +1,26 @@
 export const http = (dispatch, props) => {
-  const { done, error = props.done, method = 'GET', url, headers = {}, body } = props
+  const {
+    done,
+    error = props.done,
+    timeout = props.error || props.done,
+    responseType = 'text',
+    requestType = 'text',
+    timeoutSeconds = 10,
+    method = 'GET',
+    url,
+    headers = {},
+  } = props
 
   const xhr = new XMLHttpRequest()
+  xhr.responseType = responseType
+  xhr.timeout = timeoutSeconds * 1000
 
   let requestDone = false
 
   const handler = action => {
     if (!requestDone) {
       requestDone = true
+
       dispatch(action, xhr)
     }
   }
@@ -25,9 +38,14 @@ export const http = (dispatch, props) => {
   }
 
   xhr.onerror = () => handler(error)
+  xhr.ontimeout = () => handler(timeout)
 
   xhr.open(method, url)
-  xhr.send(body)
+  if (props.body) {
+    xhr.send(requestType === 'json' ? lib.json.stringify(props.body, null, 2) : props.body)
+  } else {
+    xhr.send()
+  }
 }
 
 export default http
